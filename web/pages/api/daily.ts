31e16f196b1cae4b7d14bd94683cb891fd3bde7c
Luke Carr <luke_jcarr@icloud.com>
2021-12-17 12:41:03 +0000

    ✨ Replaced redis with HTTP cache-control

--- web/pages/api/daily.ts
+++ web/pages/api/daily.ts
@@ -2,9 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import * as cheerio from 'cheerio'
 import AdmZip from 'adm-zip'
-import Redis from 'ioredis'
-
-const cache = typeof process.env.REDIS_URI !== 'undefined' && new Redis(process.env.REDIS_URI)
 
 const getCsvText = async () => {
   const { data } = await axios.get('https://explore-education-statistics.service.gov.uk/find-statistics/attendance-in-education-and-early-years-settings-during-the-coronavirus-covid-19-outbreak')
@@ -17,16 +14,15 @@ const getCsvText = async () => {
   const zip = new AdmZip(zipData)
   const csvText = zip.readAsText('data/table_1b_daily_attendance_in_state_schools_during_covid_19_.csv', 'utf8')
 
-  cache && await cache.set('daily_attendance', csvText, 'ex', 60 * 60 * 24)
-
   return csvText
 }
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  const csvText = (cache && await cache.get('daily_attendance')) || await getCsvText()
+  const csvText = await getCsvText()
 
   res.status(200)
     .setHeader('Content-Type', 'text/csv')
     .setHeader('Content-Disposition', 'attachment;filename=daily_attendance.csv')
+    .setHeader('Cache-Control', `public,max-age=${60 * 60 * 24},immutable`)
     .send(csvText)
 }
