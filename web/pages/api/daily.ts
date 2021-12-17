e78f303bc32b1096ef6cb40a3bc7db380970d435
Luke Carr <luke_jcarr@icloud.com>
2021-12-17 12:21:12 +0000

    ♻️ Replaced keyv with ioredis

--- web/pages/api/daily.ts
+++ web/pages/api/daily.ts
@@ -2,9 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import * as cheerio from 'cheerio'
 import AdmZip from 'adm-zip'
-import Keyv from 'keyv'
+import Redis from 'ioredis'
 
-const cache = typeof process.env.REDIS_URI !== 'undefined' ? new Keyv(process.env.REDIS_URI) : new Keyv({ store: new Map() })
+const cache = typeof process.env.REDIS_URI !== 'undefined' && new Redis(process.env.REDIS_URI)
 
 const getCsvText = async () => {
   const { data } = await axios.get('https://explore-education-statistics.service.gov.uk/find-statistics/attendance-in-education-and-early-years-settings-during-the-coronavirus-covid-19-outbreak')
@@ -17,13 +17,13 @@ const getCsvText = async () => {
   const zip = new AdmZip(zipData)
   const csvText = zip.readAsText('data/table_1b_daily_attendance_in_state_schools_during_covid_19_.csv', 'utf8')
 
-  await cache.set('daily_attendance', csvText, 1000 * 60 * 60 * 24)
+  cache && await cache.set('daily_attendance', csvText, 1000 * 60 * 60 * 24)
 
   return csvText
 }
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  const csvText = await cache.get('daily_attendance') ?? await getCsvText()
+  const csvText = (cache && await cache.get('daily_attendance')) || await getCsvText()
 
   res.status(200)
     .setHeader('Content-Type', 'text/csv')
